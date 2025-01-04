6fa9188c6fdcee6d5190e7265f1befa1fc75ef94
Fabio Colonna <fabcolonna@icloud.com>
2025-01-04 19:20:24 +0100

    edit doxy script

--- doxy_update.sh
+++ doxy_update.sh
@@ -12,16 +12,16 @@ mv Docs/html/* "$TEMP_DOCS"
 rm -rf Docs/
 
 echo "Moving html/ to gh-pages branch..."
-git checkout gh-pages
+git checkout gh-pages >/dev/null 2>&1
 rm -rf *
 mv "$TEMP_DOCS"/* .
 rm -rf "$TEMP_DOCS"
 
 echo "Committing on gh-pages branch..."
-git add .
-git commit -m "Update Doxygen documentation"
-git push origin gh-pages
+git add . >/dev/null 2>&1
+git commit -m "Update Doxygen documentation" >/dev/null 2>&1
+git push origin gh-pages >/dev/null 2>&1
 
 echo "Switching back to main branch..."
-git checkout main
+git checkout main >/dev/null 2>&1
 echo "Done!"
